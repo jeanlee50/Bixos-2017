5349cf2fc18b4395c424f290e9536d0e82c52737
jeanlee50 <jeanlee20@gmail.com>
2017-04-26 21:10:30 -0300

    Antes tarde do que nunca

--- C/Tarefa/main.c
+++ C/Tarefa/main.c
@@ -21,11 +21,19 @@ int main() {
 	wdt_disable();
 
 	for (;;) {
-		if (get_sensor(SENSOR_FD) < 500)
-			motors(255, 255);
-		else
-			motors(-100, -100);
 
+		if (get_sensor(SENSOR_FE) <= DIST && get_sensor(SENSOR_FD) <= DIST) { //se o robô adversário estiver centralizado à frente
+			while (get_sensor(SENSOR_FE) <= DIST || get_sensor(SENSOR_FD) <= DIST) { //até o robô estar totalmente desalinhado (não empurrando nada)
+				motors(255, 255);
+			}
+		}
+
+		if (get_sensor(SENSOR_LE) < 5 || get_sensor(SENSOR_LD < 5)) { //se estiver sendo empurrado pelos lados
+			motors(255, 255); //mover para frente para não travar quando for girar
+		_	delay_ms(500); //tempo movendo para frente
+		}
+
+		motors(255, -255); //robô gira e repete o ciclo até encontrar adversário novamente
 		_delay_ms(10);
 	}
 
